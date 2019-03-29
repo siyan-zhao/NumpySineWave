533272a0a48de1d5357e7e562995096734eaf6fb
Siyan Zhao <38367749+siyanLearner@users.noreply.github.com>
2019-03-28 22:17:44 -0400

    Update randomSineWave.py

--- randomSineWave.py
+++ randomSineWave.py
@@ -1,16 +1,10 @@
 import numpy as np
-from random import random
+import random
 import matplotlib.pyplot as plot
-from random import seed
 
-def random_number(seed_number):
-    # generate random numbers between 0-100 ,
-    # since no specification
-    seed(seed_number)
-    value=random()
-    return value*100
 
-def sinewave(x_left,x_right,x_dim,seed_number):
+
+def sinewave(x_left, x_right, x_dim, seed_number):
     '''This function takes in 4 arguments:
     1.x_left the left endpoint for the x range
     2.x_right the right endpoint for the x range
@@ -22,25 +16,23 @@ def sinewave(x_left,x_right,x_dim,seed_number):
     This function returns a list with 4 elements:
     [x_axis value, y_axis value,the random amplitute, the random phase]
     '''
+    random.seed(seed_number)
     # Get x values of the sine wave
-    step=(x_right-x_left)/x_dim
-    time = np.arange(x_left,x_right, step);
-    print(time)
+    time = np.linspace(x_left, x_right, num=x_dim);
     #random phase between 0-100
-    phase=random_number(seed_number)
+    phase=random.randint(0,100)
     #random amplitute between 0-100
-    amplitute=random_number(seed_number)
+    random.seed(seed_number+1)
+    amplitute=random.randint(0,100)
     # y values
     y = amplitute*np.sin(time-phase)
-    return [time,y,amplitute,phase]
+    return time, y, amplitute, phase
 
 
 #plot with seed 2, X E [-2pi,2pi],x_dim=50;
-[x_axis,y_axis,amplitute,phase]=(sinewave(-6.28,6.28,40,2))
+x_axis,y_axis,amplitute,phase=sinewave(-6.28, 6.28, 40, 3)
 plot.plot(x_axis,y_axis)
 plot.ylabel("sine wave")
 plot.grid(True, which='both')
 plot.axhline(y=0, color='k')
 plot.show()
-
-
